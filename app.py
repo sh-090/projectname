2ef730c3787ebd6266bb000c074d4cde2fbc6ba4
sh-090 <shrutitawde090@gmail.com>
2024-12-02 11:24:58 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
 # Display a simple 'Hello World' message
-st.title('Hello, Streamlit!')
-st.write('Welcome to your first Streamlit app!')
+#st.title('Hello, Streamlit!')
+#st.write('Welcome to your first Streamlit app!')
+# Access the secret
+api_key = st.secrets["general"]["api_key"]
+
+# Display it (for demonstration purposes, don't show real API keys in production)
+st.write(f"My API key is: {api_key}")
